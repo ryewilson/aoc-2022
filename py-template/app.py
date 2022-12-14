b6ad9678fce61c5ae605bf242f0d25324affa1f0
Ryan Wilson <ryan.wilson@hyland.com>
2022-12-13 22:46:29 -0500

    Adding python template

---
+++ py-template/app.py
@@ -0,0 +1,30 @@
+
+def run():
+    """main entry point
+    """
+    # Load the file 
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+    #splitLines = [process_line(x) for x in lines] 
+
+    
+def process_line(line=""):
+    # split on ,
+    bothElves = line.split(",") 
+    return ElfSector(bothElves[0]), ElfSector(bothElves[1])
+
+
+    
+class HelperClass:
+    '''Add description here'''
+    
+    def __init__(self, strRange="") -> None:
+        pass
+        
+    def __str__(self): 
+        return "implement this"
+    
+    def __repr__(self):
+        return self.__str__()
+        
